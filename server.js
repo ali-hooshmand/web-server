849d5e08c1db2e66a5bf8da1df697e9e25f1d7d4
Ali Hooshmand <hooshmand.ali@gmail.com>
2016-10-25 18:31:24 -0700

    Separating middleware file

--- server.js
+++ server.js
@@ -1,22 +1,12 @@
 var express = require('express');
 var app = express();
 var PORT = 3000;
-var middleware = {
-    requireAuthentication: function(req, res, next){
-        console.log('private route hit!');
-        next();
-    },
-    logger:function(req, res, next){
-        console.log('Request: '+req.method+ ' ' + req.originalUrl+ ' '+ new Date().toString());
-        next();
-    }
-};
+
+var middleware = require('./middleware.js');
+
 
 app.use(middleware.logger);
 
-//app.get('/',function(req, res){
-//    res.send('hello ali!');
-//});
 app.get('/about', middleware.requireAuthentication, function(req, res){
     res.send('About us...');
 });
